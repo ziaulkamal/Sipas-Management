9f17b7cbd2d3b0f520fed6887a595da11bcb0a4d
rijalulakhri-dev <shfdell@gmail.com>
2023-08-11 18:00:34 +0700

    update again

--- application/views/page/piket/Track_berkas.php
+++ application/views/page/piket/Track_berkas.php
@@ -24,7 +24,7 @@
 				<div class="card">
 					<div class="card-body">
 						<h5 class="mb-4 text-uppercase"><i class="mdi mdi-google-maps me-1"></i>
-							Tracking Suratss</h5>
+							Tracking Surat</h5>
 
 						<ul class="list-unstyled timeline-sm">
 							<li class="timeline-sm-item">
